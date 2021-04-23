d22a06a6a9ea22226f79a770005ac3a592ed537e
ixicale <ixicaleescalante@gmail.com>
2021-04-23 10:41:31 -0500

    .

--- app.py
+++ app.py
@@ -24,5 +24,6 @@ env = setup["development"]
 if config_decouple("PRODUCTION", default=False):
     env = setup["production"]
 
-app = create_app(env)
-app.run(debug=True, host="0.0.0.0")
+if __name__ == "__main__":
+    app = create_app(env)
+    app.run(debug=True, host="0.0.0.0")
