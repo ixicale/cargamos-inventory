c451a27892bd3db5a4461d82c516f54d4f497b81
ixicale <ixicaleescalante@gmail.com>
2021-04-23 11:30:58 -0500

    .

--- application/config/flask.py
+++ application/config/flask.py
@@ -20,7 +20,7 @@ class DevConfig(Config):
 
 class ProConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = f"postgres://wsttmnvroiswzi:005b8522d48e28689d73c4e8191858ace466f8d0df0d5a2e6065a21e368a2f55@ec2-23-22-191-232.compute-1.amazonaws.com:5432/dbbsn43ir085l7"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://wsttmnvroiswzi:005b8522d48e28689d73c4e8191858ace466f8d0df0d5a2e6065a21e368a2f55@ec2-23-22-191-232.compute-1.amazonaws.com:5432/dbbsn43ir085l7"
 
 
 setup = {
